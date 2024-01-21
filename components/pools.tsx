03a66fcff1bfdd99c04c413a730a2d2e6a77a531
Mohammed Aslam Saidummadath <aslamprpd@gmail.com>
2024-01-21 22:15:45 +0530

    dashboard link added

--- components/pools.tsx
+++ components/pools.tsx
@@ -213,9 +213,11 @@ export default function Pools() {
       >
         <div className="w-4/5 py-6 text-white mt-20 bg-[#1E2431] rounded-3xl">
           <table className=" w-full">
-            <thead>
-              <tr>
+            <thead  className="w-full grid col-6">
+
+              <tr >
                 {columns.map((column) => (
+
                   <th
                     className="text-[#AAB3CA] text-sm font-normal text-end"
                     key={column.key}
@@ -227,13 +229,15 @@ export default function Pools() {
             </thead>
             <tbody>
               {pools.map((pool: any, index: number) => (
-                <tr key={index}>
+                  <Link className="w-full grid col-6" href={`/pool/${pool.address}`}>
+                <tr  key={index}>
                   {columns.map((column) => (
                     <td key={column.key} className="text-end px-4">
                       {renderCell(pool, column.key)}
                     </td>
                   ))}
                 </tr>
+                  </Link>
               ))}
             </tbody>
           </table>
